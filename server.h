13bd6a46398e5805d369fbbac74e33c79e4d1281
mr108 <mr108@cs-ssh.argo.uwf.edu>
2020-02-15 15:18:08 -0600

    Commented server.h

--- server.h
+++ server.h
@@ -1,10 +1,30 @@
 #ifndef DEF_SERVER
 #define DEF_SERVER
 
+/**
+ * readFile: Reads in the contents of a file one character at a time,
+ * storing the results in dataBuff. Counts the number of chars for
+ * Content-Length. Then concatenates the pre-formatted response header
+ * (see line 19), the determined content-length, and the file data to
+ * send to the client.
+ * @Params fileName The name of the file being read in.
+ */
 void readFile(char *fileName);
 
+/**
+ * getFiles: Reads in every file in the cwd and stores their names in
+ * 'files'. Additionally tracks how many files are in the cwd.
+ */
 void getFiles();
 
+/**
+ * checkFileExists: Takes in a complete request header from the client  
+ * and determines if the requested file from the client exists. If it 
+ * exists, returns the index of the file in 'files', -1 otherwise.
+ * @Params buff The buffer containing the entire request header
+ * @Return the index of the file in 'files' if the file exists, -1
+ * otherwise
+ */
 int checkFileExists(char *buff);
 
 #endif
